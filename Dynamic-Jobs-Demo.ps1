da7885c5d9c568919ebf1da84197308e99030561
eggstoastbacon <55957368+eggstoastbacon@users.noreply.github.com>
2020-02-16 11:15:36 -0800

    Update Dynamic-Jobs-Demo.ps1

--- Dynamic-Jobs-Demo.ps1
+++ Dynamic-Jobs-Demo.ps1
@@ -3,29 +3,24 @@ function checkJobState {
     if ("Running" -in $JobStatus) { $Global:Status = "Running" }else { $Global:Status = "Finished" }
 }
 
-$d = "01", "02", "03", "04", "05", "06", "07", "08"
-$y = "01", "02", "03", "04", "05", "06", "07", "08"
-$variables = import-csv somelistofitems.text
+#Items to run per job. Increase or decrease to distribute load over job tasks.
+$items = 25
+#Spawn 8 jobs (doing $items amount each)
+$y = 0..7
+$path = "\filepath"
+$content = Import-CSV -Path $path
 
-foreach ($c in $d) {
     foreach ($x in $y) {
-        new-variable -Name ($c + "_job" + $x) -value (start-job -Name ($c + $x) -ScriptBlock {
-                param ([string]$c, [string]$x) 
-    
-                if ($x -eq "01") { $a = 0; $b = 25 }
-                if ($x -eq "02") { $a = 26; $b = 50 }
-                if ($x -eq "03") { $a = 51; $b = 75 }
-                if ($x -eq "04") { $a = 76; $b = 100 }
-                if ($x -eq "05") { $a = 101; $b = 125 }
-                if ($x -eq "06") { $a = 126; $b = 150 }
-                if ($x -eq "07") { $a = 151; $b = 175 }
-                if ($x -eq "08") { $a = 176; $b = 200 }
-
-                $variables = $variables[$global:i..$global:a]
-
-                foreach ($variable in $variables) {
-                #do something with this item.
+        new-variable -Name ($x + "_jobvar") -value (start-job -Name ($x + "_job") -ScriptBlock {
+                param ([string]$x) 
+               
+                if ($x -eq 0) { $a = 0; $b = $a + $items;write-host "true" }
+                if ($x -eq ($z + 1)) { $a = $b + 1; $b = $a + $items}
+                $z = $x
+                
+                $files = $files[$global:a..$global:b]
+                foreach ($file in $files) {
+                #Do some action
                 }  
-            } -ArgumentList ($c, $x)) 
+            } -ArgumentList ($x)) 
     }
-}
